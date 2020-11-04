677ad6b5153fdb43f829e720a8aa5e6dcd102157
CYQAQ555 <119115527@qq.com>
2020-11-04 22:41:52 +0800

    iust a test

---
+++ 2020-11-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000;
+            double p;
+            double P = 1d;
+            double a, b, c;
+            for (int i = 1; i <= n; i++)
+            {
+                a = 2 * i;
+                b = a - 1;
+                c = a + 1;
+                P *= a / b * a / c;
+            }
+            p = P * 2;
+            Console.WriteLine("Π/2近似"+p);
+        }
+    }
+}
